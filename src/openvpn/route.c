flichtenheld/openvpn

commit dca6b03098ab0f3ef808c891673305651dc6704a

route: Fix a unused-but-set-variable warning on OpenBSD
So we could enable -Werror for OpenBSD builds.

Change-Id: Ic971604beb1320d7b9d6121cd8e8519ccc1a7eb9
Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>
Acked-by: MaxF <max@max-fillinger.net>
Gerrit URL: https://gerrit.openvpn.net/c/openvpn/+/1214
Message-Id: <20251004141935.17815-1-gert@greenie.muc.de>
URL: https://sourceforge.net/p/openvpn/mailman/message/59242246/
Signed-off-by: Gert Doering <gert@greenie.muc.de>
(cherry picked from commit 9243ea88913fc1bed26c25ba7d7b18bb2f16fa4c)

src/openvpn/route.c
@@ -2358,6 +2358,7 @@ delete_route_ipv6(const struct route_ipv6 *r6, const struct tuntap *tt,
         device = r6->iface;
         gateway_needed = true;
     }
+    (void)device; /* unused on some platforms */
 
     /* if we used a gateway on "add route", we also need to specify it on
      * delete, otherwise some OSes will refuse to delete the route

commit ad5612d585f6de550f5831b68d5bf7411cdeeece

route: Add #endif comment for uncrustify compliance
Cherry-pick dca6b03098ab0f3ef808c891673305651dc6704a
broke uncrustify compliance. Apparently this one added
line pushed it over the limit to require the #endif
comment.

Change-Id: I46d0e4be82a5cc7e466d6e8f658e5b131b5401c4
Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>

src/openvpn/route.c
@@ -2368,7 +2368,7 @@ delete_route_ipv6(const struct route_ipv6 *r6, const struct tuntap *tt,
     {
         gateway_needed = true;
     }
-#endif
+#endif /* ifndef _WIN32 */
 
     struct gc_arena gc = gc_new();
     struct argv argv = argv_new();